feab4bc7f882747395ed6f4eb06a31bd0d8f22e2
Michael Nicosia <michael.nicosia@RACKSPACE.COM>
2017-08-03 02:41:32 +0000

    awelchy is alive!

---
+++ server.rb
@@ -0,0 +1,54 @@
+require 'net/http'
+require 'nokogiri'
+require 'rack'
+require 'rack/server'
+
+OK = 200
+BAD_REQUEST = 400
+TEXT_PLAIN = { "Content-Type" => "text/plain" }
+
+class Awelchy
+
+  def self.call(env)
+    req = Rack::Request.new env
+    path_info = req.env["PATH_INFO"]
+    if req.post?
+      return Awelchy.fuzzy_match(req) if Awelchy.is_fuzzy_match?(path_info)
+    end
+    return [BAD_REQUEST, TEXT_PLAIN, ['Unsupported request.']]
+  end
+
+  def self.is_fuzzy_match?(path_info)
+    path_info == "/fuzzy-match"
+  end
+
+  def self.fuzzy_match(req)
+    body = req.body.read
+    terms = body.split
+    awelchisms = Awelchy.awelchisms
+    weighed_awelchisms = Awelchy.weigh_urls(terms, awelchisms)
+    heaviest_awelchism = Awelchy.heaviest_url(weighed_awelchisms)
+    return [OK, TEXT_PLAIN, [heaviest_awelchism]]
+  end
+
+  def self.weigh_urls(terms, urls)
+    urls.map do |url| 
+      weight = 0
+      terms.each do |term|
+        weight += 1 if url.include?(term)
+      end
+      { weight: weight, url: url }
+    end
+  end
+
+  def self.heaviest_url(weighed_urls)
+    weighed_urls.sort_by { |k| k[:weight] }.last[:url]
+  end
+
+  def self.awelchisms
+    doc = Nokogiri::HTML(Net::HTTP.get('www.awelchisms.com', '/'))
+    doc.search('img').map { |img| img.attributes['src'].value }
+  end
+end
+
+Rack::Server.start :app => Awelchy
